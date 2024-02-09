f1feb75dcabfbfe9ef80b4df218a03bdc435361c
Kira McLean <email@kiramclean.com>
2024-02-08 22:12:43 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tcstats
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/scicloj/tcstats_test.clj
@@ -0,0 +1,7 @@
+(ns scicloj.tcstats-test
+  (:require [clojure.test :refer :all]
+            [scicloj.tcstats :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
